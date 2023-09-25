9364029dee1877908fa7dca6026e2f5d96d02f20
MartinZos <98450434+MartinZos@users.noreply.github.com>
2023-09-25 13:11:14 +0200

    Update Test.py v0.2
    serus

--- Test.py
+++ Test.py
@@ -1,33 +1,107 @@
 import random
+import os
 
+# Function to clear the terminal
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
 # List of sentences with answers and questions
 sentences = [
+    ("senzacny", "sensationell"),
+    ("hladat", "suchen"),
+    ("cast, diel", "der Teil"),
+    ("nezdvorily", "unhoflich"),
+    ("nejasny", "unklar"),
+    ("nepresny", "unkorrekt"),
+    ("nemoderny", "unmodern"),
+    ("neprakticky", "unpraktisch"),
+    ("podujatie", "die Veranstaltung"),
+    ("zakaz", "das Verbot"),
+    ("plny", "voll"),
+    ("predtym", "vorher"),
+    ("vpredu", "vorn"),
+    ("navrh", "der Vorschlag"),
+    ("prednaska", "der Vortrag"),
+    ("pravda", "wahr"),
+    ("dost, znacne", "ziemlich"),
+    ("medzi", "zwischen"),
+    ("metoda", "die Methode"),
+    ("sused", "der Nachbar"),
+    ("popoludnie", "der Nachmittag"),
+    ("nutny", "notig"),
+    ("organizacia", "die Organisation"),
+    ("pefektny", "perfekt"),
+    ("plan", "der Plan"),
+    ("planovat", "planen"),
+    ("planovanie", "die Planung"),
+    ("premysleny, podla planu", "planvoll"),
+    ("prezentacia", "die Prasentation"),
+    ("presny na cas", "punktlich"),
+    ("referent", "der Referent"),
+    ("policka", "das Regal"),
+    ("pravidlo", "die Regel"),
+    ("kolo", "die Runde"),
+    ("skoda", "schade"),
+    ("znacka,tabulka stitok", "das Schild"),
+    ("poznamkovy blok", "der Schreibblock"),
+    ("podivny,cudny", "seltsam"),
+    ("prijemny", "angenehm"),
+    ("strom", "der Baum"),
+    ("potom", "danach"),
+    ("jednoduchy", "einfach"),
+    ("zapisat", "eintragen"),
+    ("priatelsky", "freundlich"),
+    ("do pat", "zu funf"),
+    ("myslienka", "der Gedanke"),
+    ("presne", "genau"),
+    ("nekompetentny", "inkompetent"),
+    ("komplikovany", "kompliziert"),
+    ("dlzka", "die Lange"),
+    ("nudny", "langweilig"),
+    ("ucebnica", "das Lehrwerk"),
+    ("obsah", "der Inhalt"),
+    ("lahky", "leicht"),
+    ("vesely", "lustig"),
+    ("mienka", "die Meinung"),
     ("sam", "allein"),
-    ("vynikajúci", "ausgezeichnet"),
-    ("Bavorsko", "Bayern"),
-    ("správa", "der Bericht"),
-    ("zaoberať sa niečím", "beschäftigen sich mit etwas"),
-    ("byť zaneprázdnený", "beschäftigt sein"),
-    ("raz", "elnmal"),
-    ("fantastický", "fantastisch"),
-    ("prázdniny", "die Ferien"),
-    ("Čo tam bolo vidieť?", "Was gab es zu sehen?"),
-    ("celkom skvelý", "ganz toll"),
-    ("celý deň", "den ganzen Tag"),
+    ("vynikajuci", "ausgezeichnet"),
+    ("raz", "einmal"),
+    ("sprava", "der Bericht"),
+    ("celkom", "ganz"),
     ("proti", "gegen"),
-    ("včera", "gestern"),
-    ("nádherný", "herrlich"),
-    ("prvotriedny, vynikajúci", "klasse"),
-    ("variť", "kochen"),
-    ("posledný", "der letzte"),
+    ("vcera", "gestern"),
+    ("predvcerom", "vorgestern"),
+    ("nadherny", "herrlich"),
+    ("prvotriedny", "klasse"),
+    ("varit", "kochen"),
+    ("posledny", "der Letzte"),
     ("majster", "der Meister"),
-    ("Všetkému sa treba učit", "Übung macht den Meister"),
-    ("hráč národného družstva", "der Nationalspieler"),
-    ("organizovať", "organisieren"),
-    ("prima, výborný", "prima"),
-    ("dážď", "der Regen"),
-    ("hra s pridelenými úlohami", "das Rollenspiel"),
-    ("veta", "der Satz")
+    ("Hrac narodneho druzstva", "der Nationalspieler"),
+    ("organizovat", "organisieren"),
+    ("oci", "der Papa"),
+    ("prima", "prima"),
+    ("dazd", "der Regen"),
+    ("hra s pridelenymi ulohami", "das Rollenspiel"),
+    ("veta", "der Satz"),
+    ("plavat", "schwimmen"),
+    ("vidiet", "sehen"),
+    ("vyborny", "spitze"),
+    ("zapcha", "der Stau"),
+    ("cele hodiny", "stundenlang"),
+    ("super", "super"),
+    ("supermarket", "der Supermarkt"),
+    ("tabulka", "die Tabelle"),
+    ("bajecny", "toll"),
+    ("turnaj", "das Turnier"),
+    ("anketa", "die Umfrage"),
+    ("dovolenka", "der Urlaub"),
+    ("od do", "von bis"),
+    ("bol", "war"),
+    ("voda", "das Wasser"),
+    ("pocasie", "das Wetter"),
+    ("slovnik", "das Worterbuch"),
+    ("prekrasny/nadherny", "wunderbar"),
+    ("spat", "zuruck"),
+    ("prazdniny", "die Ferien")
 ]
 
 def quiz():
@@ -37,11 +111,13 @@ def quiz():
 
     for word, translation in sentences:
         user_input = input(f"What is the translation of '{word}'? (Press 'q' to quit): ").strip()
-
+        clear_terminal()
         if user_input.lower() == translation.lower():
-            print("True")
+            print("***************************************************")
+            print(f"True. '{translation}' is correct.")
             score += 1
         else:
+            print("***************************************************")
             print(f"False. The correct translation is '{translation}'.")
 
         if user_input.lower() == 'q':
@@ -56,7 +132,7 @@ while True:
     print("2. Quit")
 
     choice = input("Enter your choice (1/2): ").strip()
-
+    clear_terminal()
     if choice == '1':
         quiz()
     elif choice == '2':
