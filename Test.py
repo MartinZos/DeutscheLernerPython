9bdd4d0d5dda53145463c9a54d484c68c640846d
MartinZos <98450434+MartinZos@users.noreply.github.com>
2023-09-26 23:25:46 +0200

    v0.3
    Added interface

--- Test.py
+++ Test.py
@@ -1,9 +1,7 @@
 import random
-import os
+import tkinter as tk
+from tkinter import font as tkfont
 
-# Function to clear the terminal
-def clear_terminal():
-    os.system('cls' if os.name == 'nt' else 'clear')
 # List of sentences with answers and questions
 sentences = [
     ("senzacny", "sensationell"),
@@ -103,40 +101,76 @@ sentences = [
     ("spat", "zuruck"),
     ("prazdniny", "die Ferien")
 ]
+class QuizApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("German Vocabulary Quiz")
+        self.root.geometry("600x400")  # Increase the window size
 
-def quiz():
-    score = 0
-    total = len(sentences)
-    random.shuffle(sentences)
+        self.score = 0
+        self.current_question_index = 0
 
-    for word, translation in sentences:
-        user_input = input(f"What is the translation of '{word}'? (Press 'q' to quit): ").strip()
-        clear_terminal()
-        if user_input.lower() == translation.lower():
-            print("***************************************************")
-            print(f"True. '{translation}' is correct.")
-            score += 1
+        self.question_label = tk.Label(root, text="", font=("Arial", 16, "bold"))  # Make text bold and bigger
+        self.question_label.pack(pady=20)
+
+        self.answer_entry = tk.Entry(root, font=("Arial", 14))  # Make text bigger
+        self.answer_entry.pack(pady=10)
+        self.answer_entry.bind("<Return>", lambda event: self.check_answer())
+        self.root.bind("<Right>", lambda event: self.next_question())  # Bind right arrow key to "Next Question"
+
+        self.check_button = tk.Button(root, text="Check", command=self.check_answer, font=("Arial", 12))
+        self.check_button.pack(pady=5)
+
+        self.next_question_button = tk.Button(root, text="Next Question", command=self.next_question, font=("Arial", 12))
+        self.next_question_button.pack(pady=5)
+
+        self.quit_button = tk.Button(root, text="Quit", command=root.quit, font=("Arial", 12))
+        self.quit_button.pack(pady=20)
+
+        # Create buttons for special characters
+        special_chars = "ÄÖÜẞäöüß"
+        special_char_frame = tk.Frame(root)
+        special_char_frame.pack()
+
+        for char in special_chars:
+            char_button = tk.Button(special_char_frame, text=char, command=lambda c=char: self.insert_special_char(c),
+                                    font=("Arial", 14), width=3, height=1, padx=5, pady=5)
+            char_button.pack(side=tk.LEFT)
+
+        self.update_question()
+
+    def update_question(self):
+        if self.current_question_index < len(sentences):
+            self.question_label.config(text=f"What is the translation of '{sentences[self.current_question_index][0]}'?")
+            self.answer_entry.delete(0, tk.END)
+            self.question_label.config(fg="black")  # Reset text color
         else:
-            print("***************************************************")
-            print(f"False. The correct translation is '{translation}'.")
+            self.question_label.config(text=f"You got {self.score} out of {len(sentences)} correct.")
+            self.answer_entry.config(state=tk.DISABLED)
+            self.check_button.config(state=tk.DISABLED)
+            self.next_question_button.config(state=tk.DISABLED)
+            self.quit_button.config(state=tk.DISABLED)
 
-        if user_input.lower() == 'q':
-            break
+    def check_answer(self):
+        user_input = self.answer_entry.get().strip()
+        correct_answer = sentences[self.current_question_index][1].lower()
 
-    print(f"You got {score} out of {total} correct.")
+        if user_input.lower() == correct_answer:
+            self.question_label.config(text=f"Correct. '{correct_answer}' is correct.", fg="green")
+            self.score += 1
+        else:
+            self.question_label.config(text=f"Incorrect. The correct translation is '{correct_answer}'.", fg="red")
 
+    def next_question(self):
+        self.current_question_index += 1
+        self.update_question()
 
-while True:
-    print("Menu:")
-    print("1. Start Quiz")
-    print("2. Quit")
+    def insert_special_char(self, char):
+        current_text = self.answer_entry.get()
+        self.answer_entry.delete(0, tk.END)
+        self.answer_entry.insert(0, current_text + char)
 
-    choice = input("Enter your choice (1/2): ").strip()
-    clear_terminal()
-    if choice == '1':
-        quiz()
-    elif choice == '2':
-        print("Goodbye!")
-        break
-    else:
-        print("Invalid choice. Please enter 1 or 2.")
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = QuizApp(root)
+    root.mainloop()
