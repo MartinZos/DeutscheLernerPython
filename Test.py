be86b65ac5531e0fe2010c69ae530e9dbc56be23
MartinZos <98450434+MartinZos@users.noreply.github.com>
2023-09-29 00:02:58 +0200

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,101 +1,146 @@
-import tkinter as tk
-from tkinter import filedialog
-import json
-import os
 import random
+import tkinter as tk
+from tkinter import font as tkfont
+from tkinter import simpledialog  # Import simpledialog for input dialog
+from random import sample
+# List of sentences with answers and questions
+#ÄÖÜẞäöüß
+sentences = [
+    ("senzacny", "sensationell"),
+    ("cast, diel", "der Teil"),
+    ("nejasny", "unklar"),
+    ("nepresny", "unkorrekt"),
+    ("nemoderny", "unmodern"),
+    ("neprakticky", "unpraktisch"),
+    ("podujatie", "die Veranstaltung"),
+    ("zakaz", "das Verbot"),
+    ("plny", "voll"),
+    ("predtym", "vorher"),
+    ("vpredu", "vorn"),
+    ("navrh", "der Vorschlag"),
+    ("prednaska", "der Vortrag"),
+    ("pravda", "wahr"),
+    ("dost, znacne", "ziemlich"),
+    ("medzi", "zwischen"),
+    ("metoda", "die Methode"),
+    ("sused", "der Nachbar"),
+    ("popoludnie", "der Nachmittag"),
+    ("nutny", "notig"),
+    ("organizacia", "die Organisation"),
+    ("pefektny", "perfekt"),
+    ("plan", "der Plan"),
+    ("planovat", "planen"),
+    ("planovanie", "die Planung"),
+    ("premysleny, podla planu", "planvoll"),
+    ("prezentacia", "die Prasentation"),
+    ("presny na cas", "punktlich"),
+    ("referent", "der Referent"),
+    ("policka", "das Regal"),
+    ("pravidlo", "die Regel"),
+    ("kolo", "die Runde"),
+    ("skoda", "schade"),
+    ("znacka,tabulka stitok", "das Schild"),
+    ("poznamkovy blok", "der Schreibblock"),
+    ("podivny,cudny", "seltsam"),
+    ("prijemny", "angenehm"),
+    ("strom", "der Baum"),
+    ("potom", "danach"),
+    ("jednoduchy", "einfach"),
+    ("zapisat", "eintragen"),
+    ("priatelsky", "freundlich"),
+    ("do pat", "zu funf"),
+    ("myslienka", "der Gedanke"),
+    ("presne", "genau"),
+    ("nekompetentny", "inkompetent"),
+    ("komplikovany", "kompliziert"),
+    ("dlzka", "die Lange"),
+    ("nudny", "langweilig"),
+    ("ucebnica", "das Lehrwerk"),
+    ("obsah", "der Inhalt"),
+    ("lahky", "leicht"),
+    ("vesely", "lustig"),
+    ("mienka", "die Meinung"),
+    ("celkom", "ganz"),
+    ("predvcerom", "vorgestern"),
+    ("nezdvorily", "unhoflich"),
+    ("Prvotna informacia", "die Erstinformation"),
+]
+#sentences = dict(zip(sentences, sample(list(sentences.values()),len(sentences))))
+sentences = random.sample(sentences, len(sentences))
+class QuizApp:
+    def __init__(self, root, num_questions):
+        self.root = root
+        self.root.title("German Vocabulary Quiz")
+        self.root.geometry("600x400")  # Increase the window size
 
-sentences = []
+        self.num_questions = num_questions
+        self.score = 0
+        self.current_question_index = 0
 
-def import_button_clicked():
-    global sentences, status_label  # Declare 'sentences' and 'status_label' as global variables
-    sentences = []  # Initialize 'sentences' as an empty list
+        self.question_label = tk.Label(root, text="", font=("Arial", 16, "bold"))  # Make text bold and bigger
+        self.question_label.pack(pady=20)
 
-    # Open a file dialog to select a JSON file
-    file_path = filedialog.askopenfilename(filetypes=[("JSON files", "*.json")])
+        self.answer_entry = tk.Entry(root, font=("Arial", 14))  # Make text bigger
+        self.answer_entry.pack(pady=10)
+        self.answer_entry.bind("<Return>", lambda event: self.check_answer())
+        self.root.bind("<Right>", lambda event: self.next_question())  # Bind right arrow key to "Next Question"
 
-    if file_path:
-        try:
-            with open(file_path, "r", encoding="utf-8") as file:
-                # Check if the file is empty
-                file_contents = file.read()
-                if not file_contents:
-                    status_label.config(text="JSON file is empty.", fg="red")
-                    return sentences
+        self.check_button = tk.Button(root, text="Check", command=self.check_answer, font=("Arial", 12))
+        self.check_button.pack(pady=5)
 
-                # Load JSON data from the selected file into a list
-                word_pairs = json.loads(file_contents)
+        self.next_question_button = tk.Button(root, text="Next Question", command=self.next_question, font=("Arial", 12))
+        self.next_question_button.pack(pady=5)
 
-                # Append word pairs to 'sentences' list
-                sentences.extend(word_pairs)
+        self.quit_button = tk.Button(root, text="Quit", command=root.quit, font=("Arial", 12))
+        self.quit_button.pack(pady=20)
 
-                # Extract the file name without the suffix
-                file_name_without_extension = os.path.splitext(os.path.basename(file_path))[0]
+        # Create buttons for special characters
+        special_chars = "ÄÖÜẞäöüß"
+        special_char_frame = tk.Frame(root)
+        special_char_frame.pack()
 
-                status_label.config(text=f".json {file_name_without_extension} loaded successfully", fg="green")
-                print(f".json {file_name_without_extension} loaded successfully.")
-                return sentences
-        except FileNotFoundError:
-            status_label.config(text="File not found: " + file_path, fg="red")
-            print(f"File not found: {file_path}")
-        except json.JSONDecodeError as e:
-            status_label.config(text="Error decoding JSON file: " + str(e), fg="red")
-            print(f"Error decoding JSON file: {e}")
-        except Exception as e:
-            status_label.config(text="An error occurred: " + str(e), fg="red")
-            print(f"An error occurred: {e}")
-    else:
-        status_label.config(text="No file selected.", fg="red")
-        print("No file selected.")
+        for char in special_chars:
+            char_button = tk.Button(special_char_frame, text=char, command=lambda c=char: self.insert_special_char(c),
+                                    font=("Arial", 14), width=3, height=1, padx=5, pady=5)
+            char_button.pack(side=tk.LEFT)
 
-    return sentences
+        self.update_question()
 
-def clear_window():
-    # Clear the window
-    for widget in root.winfo_children():
-        widget.destroy()
+    def update_question(self):
+        if self.current_question_index < self.num_questions:
+            self.question_label.config(text=f"What is the translation of '{sentences[self.current_question_index][0]}'?")
+            self.answer_entry.delete(0, tk.END)
+            self.question_label.config(fg="black")  # Reset text color
+        else:
+            self.question_label.config(text=f"You got {self.score} out of {self.num_questions} correct.")
+            self.answer_entry.config(state=tk.DISABLED)
+            self.check_button.config(state=tk.DISABLED)
+            self.next_question_button.config(state=tk.DISABLED)
+            self.quit_button.config(state=tk.NORMAL)  # Allow quitting
 
-def start_button_clicked():
-    # Check if 'sentences' list is empty
-    if not sentences:
-        status_label.config(text=".json file not loaded", fg="red")
-        return
+    def check_answer(self):
+        user_input = self.answer_entry.get().strip()
+        correct_answer = sentences[self.current_question_index][1].lower()
 
-    # Clear the window
-    clear_window()
+        if user_input.lower() == correct_answer:
+            self.question_label.config(text=f"Correct. '{correct_answer}' is correct.", fg="green")
+            self.score += 1
+        else:
+            self.question_label.config(text=f"Incorrect. The correct translation is '{correct_answer}'.", fg="red")
 
-    # Create a label and entry for entering the number of questions
-    num_questions_label = tk.Label(root, text="Enter the number of questions:")
-    num_questions_entry = tk.Entry(root)
-    submit_button = tk.Button(root, text="Submit", command=lambda: submit_button_clicked(num_questions_entry.get()))
+    def next_question(self):
+        self.current_question_index += 1
+        self.update_question()
 
-    # Place the widgets on the window
-    num_questions_label.pack()
-    num_questions_entry.pack()
-    submit_button.pack()
+    def insert_special_char(self, char):
+        current_text = self.answer_entry.get()
+        self.answer_entry.delete(0, tk.END)
+        self.answer_entry.insert(0, current_text + char)
 
+if __name__ == "__main__":
+    root = tk.Tk()
+    num_questions = simpledialog.askinteger("Number of Questions", "How many questions do you want to answer?")
 
-# Create the main window
-root = tk.Tk()
-root.title("Button Window")
-
-# Set the initial size of the window (width x height)
-root.geometry("600x400")
-
-# Create and configure buttons with larger size
-button_width = 10
-button_height = 2
-start_button = tk.Button(root, text="Start", command=start_button_clicked, width=button_width, height=button_height)
-import_button = tk.Button(root, text="Import", command=import_button_clicked, width=button_width, height=button_height)
-quit_button = tk.Button(root, text="Quit", command=root.destroy, width=button_width, height=button_height)
-
-# Create a label for displaying status messages
-status_label = tk.Label(root, text="", font=("Helvetica", 12))
-
-# Place the buttons and status label
-status_label.pack(pady=20)
-start_button.pack()
-import_button.pack()
-quit_button.pack()
-
-# Start the tkinter main loop
-root.mainloop()
+    if num_questions is not None:
+        app = QuizApp(root, num_questions)
+        root.mainloop()
